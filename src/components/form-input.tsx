338c98091095a76e226862a87ce50d48f0e40ac9
zoasr <44146518+ZOASR@users.noreply.github.com>
2025-07-15 02:10:44 +0300

    refactor: Improve form input component styling and accessibility

--- src/components/form-input.tsx
+++ src/components/form-input.tsx
@@ -3,14 +3,15 @@ import { cn } from '$/lib/utils';
 
 export const FormInput = ({ className, required, ...props }: React.ComponentProps<typeof Input>) => {
 	return (
-		<span
+		// biome-ignore lint/a11y/noLabelWithoutControl: false positive
+		<label
 			className={cn(
 				'flex items-center px-4 py-3 w-full h-auto rounded-md border-0 transition-all bg-skin-secondary text-skin-text-2/50 selection:bg-skin-secondary-2 placeholder:text-skin-text-2/30 has-focus:border-transparent has-focus:text-skin-text-2 has-focus:ring-2 has-focus:ring-skin-secondary-2',
 				className
 			)}
 		>
-			<Input className="bg-transparent border-0 focus-visible:ring-0" {...props} />
+			<Input className="p-0 bg-transparent border-0 dark:bg-transparent focus-visible:ring-0" {...props} />
 			{required && <span className="text-red-500">*</span>}
-		</span>
+		</label>
 	);
 };
