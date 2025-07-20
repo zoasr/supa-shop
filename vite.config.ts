4da5b3b1f7e02b171253e295c6e4105c4b04128d
zoasr <44146518+ZOASR@users.noreply.github.com>
2025-07-20 18:56:14 +0300

    deploy: deployed to cloudflare workers
    This commit introduces Cloudflare Workers for streamlined deployment and enhances functionality by adding a basic API endpoint. It also includes configuration and dependency updates.

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss(), svgr()],
+	plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss(), svgr(), cloudflare()],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
