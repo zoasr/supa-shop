7c17352f3bbe209606833c05b48e14a99b5e865c
zoasr <44146518+ZOASR@users.noreply.github.com>
2025-07-22 20:06:12 +0300

    cron(cf): add the scheduled function to execute the cron job correctly

--- worker/index.ts
+++ worker/index.ts
@@ -3,8 +3,30 @@ export interface Env {
 	VITE_SUPABASE_KEY: string;
 }
 export default {
-	async fetch(_, env) {
-		// const url = new URL(request.url);
+	async fetch(request, env) {
+		const url = new URL(request.url);
+        if(url.pathname === "/cron/") {
+
+            const response = await fetch(
+                `${env.VITE_PROJECT_URL}/rest/v1/products?select=category`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        apikey: env.VITE_SUPABASE_KEY,
+                    },
+                }
+            );
+            if (response.ok) {
+                console.log(`hit the supabase ${env.VITE_PROJECT_URL} database successfully`);
+            }
+
+            return Response.json({
+                message: `hit the supabase ${env.VITE_PROJECT_URL} database successfully`,
+            });
+        }
+        return new Response("Not found", { status: 404 });
+	},
+    async scheduled(_, env) {
         const response = await fetch(
             `${env.VITE_PROJECT_URL}/rest/v1/products?select=category`,
             {
@@ -17,9 +39,5 @@ export default {
         if (response.ok) {
             console.log(`hit the supabase ${env.VITE_PROJECT_URL} database successfully`);
         }
-
-        return Response.json({
-            message: `hit the supabase ${env.VITE_PROJECT_URL} database successfully`,
-        });
-	},
+    }
 } satisfies ExportedHandler<Env>;
